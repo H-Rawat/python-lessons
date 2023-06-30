2d386088045651d2a827189c558997cd1d914a06
H-Rawat <hrawat9958@gmail.com>
2023-06-30 23:37:43 +0530

    add code for multiple assignments

---
+++ assignment.py
@@ -0,0 +1,9 @@
+# name = "bro"
+# age = 21
+# attractive = True
+
+# name, age, attractive = 'bro', 21, True
+# print(name, age, attractive)
+
+# luffy = zoro = naruto = sasuke = 30
+# print(luffy, zoro, naruto, sasuke)
