3cc6aa12c2c4ff30812698b857236a23e17d90b4
H-Rawat <hrawat9958@gmail.com>
2023-07-01 19:23:47 +0530

    for loop

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+# print numbers from 1 to 10
+# for i in range(10):
+#     print(i + 1)
+
+# for i in range(50, 101, 2):
+#     print(i)
+
+# for i in 'Bro Code':
+#     print(i)
+
+# COUNTDOWN TO ZERO
+# -----------------
+
+# import time
+# for seconds in range(10, 0, -1):
+#     print(seconds)
+#     time.sleep(1)
+# print('yay!')
