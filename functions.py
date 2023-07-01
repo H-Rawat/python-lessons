5a7ae9dd5ea8a10abcf985ecfcd5c6b961d4788c
H-Rawat <hrawat9958@gmail.com>
2023-07-01 23:16:41 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+# a block of code, which is executed when its called.
+
+def hello(first_name, last_name, age):
+    print('hello!', first_name, last_name, age)
+    print('have a nice day.')
+
+
+hello('messi')
+hello('dude')
+
+fake_name = 'luffy'
+last_name = 'd'
+hello(fake_name, last_name, 21)
