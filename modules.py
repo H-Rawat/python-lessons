6eaea7e8dfa388c19af5b187fba7bbe870b429b9
H-Rawat <hrawat9958@gmail.com>
2023-07-08 23:02:48 +0530

    modules

---
+++ modules.py
@@ -0,0 +1,22 @@
+# file containing python code.
+# used with modular programming.
+
+def hello():
+    print('hello')
+
+
+def bye():
+    print('bye')
+
+# i can import this module like this in some other file
+# import messages OR import messages as msg
+# messages.hello() OR msg.hello()
+# messages.bye() OR msg.bye()
+
+# to import specific functions from a file
+# from messages import hello, bye
+# from messages import * --> not recommended
+
+
+# pre-written modules
+help('modules')
