40e9474e0c209f8b6494feecbfdb5e707bd3ca57
H-Rawat <hrawat9958@gmail.com>
2023-07-09 00:05:05 +0530

    class variables

--- OOP/car.py
+++ OOP/car.py
@@ -1,12 +1,16 @@
 class Car:
+    # class variable
+    wheels = 4
+
     def __init__(self, make, model, year, color):
+        # these are instance variables
         self.make = make
         self.model = model
         self.year = year
         self.color = color
 
-    def drive(self):
-        print('This ' + self.model + ' is driving')
+    # def drive(self):
+    #     print('This ' + self.model + ' is driving')
 
-    def stop(self):
-        print('This ' + self.model + ' car is stopped')
+    # def stop(self):
+    #     print('This ' + self.model + ' car is stopped')
--- OOP/oop.py
+++ OOP/oop.py
@@ -4,6 +4,13 @@ car1 = Car('chevy', 'corvet', 2021, 'blue')
 car2 = Car('Ford', 'Mustang', 2022, 'red')
 
 # print(car1.color)
-
 # car1.drive()
 # car1.stop()
+
+# car1.wheels = 2
+# print(car1.wheels)
+# print(Car.wheels)
+
+# Car.wheels = 2
+# print(car1.wheels)
+# print(car2.wheels)
