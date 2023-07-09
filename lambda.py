96f45ea4bd652d09b1ad5cce2453f73f6aef6df5
H-Rawat <hrawat9958@gmail.com>
2023-07-09 18:52:08 +0530

    lambda function

---
+++ lambda.py
@@ -0,0 +1,25 @@
+# function written in 1 line
+# accepts any number of arguments but only has one expression
+# think of it as a shortcut
+
+# lambda parameters:expression
+
+# def double(x):
+#     return x * 2
+
+
+# print(double(6))
+
+# lambda returns a function obj
+# double = lambda x: x*2
+# print(double(5))
+
+# multiply = lambda x,y: x * y
+# print(multiply(2, 4))
+
+# full_name = lambda first_name, last_name: first_name + " " + last_name
+# print(full_name('bro', 'code'))
+
+age_check = lambda age: True if age >= 18 else False
+# print(age_check(12))
+# print(age_check(19))
