c5fa9a565d4868c20fcb28d3c3cebc76a233729a
H-Rawat <hrawat9958@gmail.com>
2023-07-09 11:02:09 +0530

    function to a variable

---
+++ func_to_var.py
@@ -0,0 +1,13 @@
+# def hello():
+#     print('hello')
+
+
+# hello()
+# print(hello)
+# hi = hello
+# print(hi)
+# hi()
+
+# -------------------------
+say = print
+say('hello, i am copy of print function')
